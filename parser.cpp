16d43edcddaaa62760aa9f5cdf6cc22225cc0b3c
Tom <tomcrasset@gmail.com>
2017-12-23 11:00:16 +0100

    Error during average, fixed !

--- parser.cpp
+++ parser.cpp
@@ -410,11 +410,7 @@ bool Parser::infixToRPN(Exp& exp,std::vector<Lexer::token>& tokenVector){
  	
  	exp.clear();
 
- 	//Variables to keep track of the parentheses for AVG
- 	int parentheses = 0;
- 	std::vector<int> avgPar;
-
- 	auto tok = tokenVector.begin();
+ 	std::vector<Lexer::token>::iterator tok = tokenVector.begin();
  	while(tok != tokenVector.end()){
 
 		if(*tok == Lexer::PI || *tok == Lexer::X || *tok == Lexer::Y){
@@ -428,18 +424,29 @@ bool Parser::infixToRPN(Exp& exp,std::vector<Lexer::token>& tokenVector){
 
 		}
 		else if(*tok == Lexer::AVG){
-			//Remember the number of parentheses
-			avgPar.push_back(parentheses);
+			operatorStack.push_back(tokenToText[*tok]);
 		}
 		else if(*tok == Lexer::COMMA){
-			//Do nothing
+			//As long as the top of the stack is SIN,COS oe AVG, put it on the expression
+			while(!operatorStack.empty() && (operatorStack.back() == tokenToText[Lexer::SIN] ||
+											operatorStack.back() == tokenToText[Lexer::	COS] ||
+											operatorStack.back() == tokenToText[Lexer::AVG])){
+				if(operatorStack.back() == tokenToText[Lexer::AVG]){
+					exp.push_back("+");
+					exp.push_back("2");
+					exp.push_back("/");
+				}else{
+					exp.push_back(operatorStack.back());
+				}
+
+				operatorStack.pop_back();
+			}
+			
 		}
 		else if(*tok == Lexer::OPEN_PAR){
-			parentheses++;
 			operatorStack.push_back(tokenToText[*tok]);
 		}
 		else if(*tok == Lexer::CLOSE_PAR){
-			parentheses--;
 
 			//Pop the operators onto the queue as long as
 			//we don't encounter an OPEN_PAR
@@ -452,15 +459,6 @@ bool Parser::infixToRPN(Exp& exp,std::vector<Lexer::token>& tokenVector){
 			if(!operatorStack.empty())
 				operatorStack.pop_back();
 
-			//If parentheses is the same as one of avgPar, we must add "+2/"
-			//(Same method as if the one for the other operators)
-			if(std::find(avgPar.begin(), avgPar.end(), parentheses) != avgPar.end()){
-
-				exp.push_back("+");
-				exp.push_back("2");
-				exp.push_back("/");
-			}
-			
 
 		//For operators (SIN,COS,TIMES)
 		}else{
@@ -485,10 +483,18 @@ bool Parser::infixToRPN(Exp& exp,std::vector<Lexer::token>& tokenVector){
 
 	// while there are still operator tokens on the stack, pop them onto the expression
 	while(!operatorStack.empty()){
-		exp.push_back(operatorStack.back());
+		if(operatorStack.back() == tokenToText[Lexer::AVG]){
+			exp.push_back("+");
+			exp.push_back("2");
+			exp.push_back("/");
+		}else{
+			exp.push_back(operatorStack.back());
+		}
+
 		operatorStack.pop_back();
 	}
 
 	return true;
 }
+
 
